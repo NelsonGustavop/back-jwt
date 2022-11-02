00396f4cf8ed511a79788ee19437653c161eb8ad
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-11-02 11:59:52 -0300

    Enum prioridade criado

--- src/main/java/com/Slayer/aot/domain/enums/Prioridade.java
+++ src/main/java/com/Slayer/aot/domain/enums/Prioridade.java
@@ -1,11 +1,11 @@
 package com.Slayer.aot.domain.enums;
 
-public enum Status {
-	ABERTO(0,"ABERTO"), ANDAMENTO(1,"ANDAMENTO") ,FECHADO(2,"ENCERRADO");
+public enum Prioridade {
+	BAIXA(0,"BAIXA"), MEDIA(1,"MEDIA") ,ALTA(2,"ALTA");
 	
 	private Integer codigo;
 	private String descricao;
-	private Status(Integer codigo, String descricao) {
+	private Prioridade(Integer codigo, String descricao) {
 		this.codigo = codigo;
 		this.descricao = descricao;
 	}
@@ -16,11 +16,11 @@ public enum Status {
 		return descricao;
 	}
 	 
-	public static Status toEnum(Integer cod) {
+	public static Prioridade toEnum(Integer cod) {
 		if(cod==null) {
 			return null;
 		}
-		for (Status x :Status.values()) {
+		for (Prioridade x :Prioridade.values()) {
 			if(cod.equals(x.getCodigo())) {
 				return x;
 			}
