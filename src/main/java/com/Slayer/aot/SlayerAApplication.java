53e4d5e840fbaf15b399671f932cb0cd66cbf5a8
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-11-02 11:44:38 -0300

    First commit

---
+++ src/main/java/com/Slayer/aot/SlayerAApplication.java
@@ -0,0 +1,13 @@
+package com.Slayer.aot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlayerAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlayerAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Slayer/aot/SlayerAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Slayer.aot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlayerAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
